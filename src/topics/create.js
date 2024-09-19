6150430747b308f5d4e375f32b31a52bb91442aa
Shanting Hou <shantinghhou@gmail.com>
2024-09-19 10:33:46 -0400

    add to backend

--- src/topics/create.js
+++ src/topics/create.js
@@ -33,6 +33,7 @@ module.exports = function (Topics) {
 			lastposttime: 0,
 			postcount: 0,
 			viewcount: 0,
+			isannonymous: data.postAnnonymous,
 		};
 
 		if (Array.isArray(data.tags) && data.tags.length) {
