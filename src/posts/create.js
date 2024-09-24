ef2ae484ad5263e3cd9e4bc286ece1cbac893580
Shanting Hou <shantinghhou@gmail.com>
2024-09-24 11:20:03 -0400

    fix errors

--- src/posts/create.js
+++ src/posts/create.js
@@ -36,7 +36,7 @@ module.exports = function (Posts) {
 			tid: tid,
 			content: content,
 			timestamp: timestamp,
-			isAnonymous: isAnonymous ? isAnonymous : false,
+			isAnonymous: isAnonymous,
 		};
 
 		if (data.toPid) {
